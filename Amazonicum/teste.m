871f1c0cfd6a95e625810335c050af683612618e
SAMANTHA-ABREU <66143734+SAMANTHA-ABREU@users.noreply.github.com>
2022-02-16 17:12:30 -0300

    Update teste.m

--- Amazonicum/teste.m
+++ Amazonicum/teste.m
@@ -7,9 +7,7 @@ data = zeros(7,length(zoomfactor));
 
 for i = 1:length(zoomfactor)
     par.z = zoomfactor(i);
-    %L_po = [5.5, 5.5, 6.3, 8.7, 8.7]; % cm, comprimento póstorbital
-    %L = L_po(1);
-    t_experiment = 30; % d, experiment durantion (Augusto, 2016)
+    t_experiment = 30;                                % d, experiment durantion (Augusto, 2016)
 
     cPar = parscomp_st(par); vars_pull(par); 
     vars_pull(cPar);  %vars_pull(data);  vars_pull(auxData);
@@ -18,50 +16,69 @@ for i = 1:length(zoomfactor)
     [t_j, t_p, t_b, l_j, l_p, l_b, l_i, rho_j, rho_B, info] = get_tj(pars_tj, f);
 
     % Corrections
-    t = t_p + t_experiment; % d, estimated age (Augusto, 2016)
+    t = (t_p + t_experiment);                         % d, estimated age (Augusto, 2016)
     Temp = C2K(30);
     TC = tempcorr(Temp, T_ref, T_A);
 
-    % Massa ganha
+    % Mass gained
     kT_M = k_M * TC;
     rT_B = rho_B * kT_M;
-    %L_m = v/ k_M/ g;
-    %L_i = L_m * l_i;
-    L = l_i - (l_i-l_p) * exp(-rT_B*t); % growth curve
-    Ww0 = L.^3 * (1 + f * ome);
-    Ww = Ww0 * (L * del_M).^3 * (1 + f * ome);
-    Wg = Ww/Ww0;                  % Massa ganha
+    L_b = L_m * l_b;
+    L_p = L_m * l_p;
+    L_i = L_m * l_i;
+    L = L_i - (L_i-L_b) * exp(-rT_B*t);               % growth curve
+    Ww0 = L_p.^3 * (1 + f * ome);
+    Ww = L.^3 * (1 + f * ome);
+%     Ww = Ww0 * (L * del_M).^3 * (1 + f * ome);
+    Wg = Ww/Ww0;                                      % Mass gained
 
-    % P - massa ganha diária
+    % P - daily growth
     Wg_d = (Ww-Ww0)/30;
 
+    % Ingestion rate
+    s_M=(l_j/l_b);                                    % Acceleration factor
+    pT_Xm = TC * p_Xm * s_M;                          % J/d.cm^2, {p_Xm} ingestion rate (Magallana gigas)
+    JT_XAm = L^2 * f * pT_Xm;                         % feeding rate per individual (Magallana gigas)
+        
+    %OU
+    JT_XAm = p_Am * L.^2/ kap_X/ mu_X * w_X/ d_X * TC;  % g/d, ingested food (Fenneropenaeus chinensis)
+    
+    %OU
+    s_M=(l_j/l_b);
+    JT_XAm = L^2 * J_X_Am * f * TC * s_M;               % mol/d, ingestion rate limited by salinity (Crassostrea corteziensis)
+    
     % Assimilação
-    J_E_Am  = p_Am/ mu_E;          % mol/d.cm^2, {J_EAm}, fluxo máximo de assimilação por superficie aréa 
+    J_E_Am  = p_Am/ mu_E;          
+    JT_EAm = TC * J_E_Am * L^2;                       % mol/d.cm^2, {J_EAm}, max assimilation flux per area surface
 
-    % Taxa de ingestão
-    y_E_X  = kap_X * mu_X/ mu_E;  % mol/mol, rendimento de reserva em alimentos
-    y_X_E  = 1/ y_E_X;            % mol/mol, rendimento de alimentos em reserva
-    J_X_Am = y_X_E * J_E_Am;      % mol/d.cm^2, {J_XAm}, fluxo máximo de digestão por superficie aréa 
-
-    % Fezes
-    J_f_Am = J_X_Am - J_E_Am;     %mol/d.cm^2, fluxo máximo de produção de fezes por superficie aréa 
-
-    % flux data 
+    %OU
+    JT_EAm = TC * p_Am * L_m^2;                      % Mytilus edulis
+    
+    %OU
+    JT_EAm = kap_X * pT_Xm;                            % (Kooijman, 2008)pag 35
+        
+    % Feces
+    J_f_Am = JT_XAm - JT_EAm;                         % mol/d.cm^2, max faeces production per surface area 
+    
+    %OU
+    TJ_X = mu_F* p_Am* TC* L^2 / kap_X;               % Hyalella azteca (Não temos o parametro mu_F, tenho que encontrar uma forma de estima-lo)
+    
+    % Flux data 
     pars_p = [kap; kap_R; g; k_J; k_M; L_T; v; U_Hb; U_Hj; U_Hp]; % compose pars
-    p_ref = p_Am * L_m^2; % J/d, capacidade de assimilação máxima no tamanho máximo
+    p_ref = TC * p_Am * L_m^2;                        % J/d, max assimilation capacity at maximum size
 
-    % respiration 
+    % Respiration 
     pACSJGRD = p_ref * scaled_power_j(L, f, pars_p, l_b, l_j, l_p); % J/d, powers
-    pACSJGRD(:,1) = 0 * pACSJGRD(:,1);                % exclui a contribuição da assimilação
-    J_M = - (n_M\n_O) * eta_O * pACSJGRD(:, [1 7 5])';% mol/d: J_C, J_H, J_O, J_N em linhas
-    EJO = - J_M(3,:)' * TC * 31.98;         % g O2/d, consumo de O2
+    pACSJGRD(:,1) = 0 * pACSJGRD(:,1);                % exclude contribution from assimilation
+    J_M = - (n_M\n_O) * eta_O * pACSJGRD(:, [1 7 5])';% mol/d: J_C, J_H, J_O, J_N in rows
+    EJO = - J_M(3,:)' * TC * 31.98;                   % g O2/d, O2 consumption
 
-    % excretion
+    % Excretion
     pACSJGRD = p_ref * scaled_power_j(L, f, pars_p, l_b, l_j, l_p); % J/d, powers
-    pACSJGRD(:,1) = 0 * pACSJGRD(:,1); % exclui a contribuição da assimilação
-    J_M = - (n_M\n_O) * eta_O * pACSJGRD(:, [1 7 5])';% mol/d: J_C, J_H, J_O, J_N em linhas
-    EJN = J_M(4,:)' * TC * 14e3;             % µg-at NH3/d, produção de amonia
+    pACSJGRD(:,1) = 0 * pACSJGRD(:,1);                % exclude contribution from assimilation
+    J_M = - (n_M\n_O) * eta_O * pACSJGRD(:, [1 7 5])';% mol/d: J_C, J_H, J_O, J_N in rows
+    EJN = J_M(4,:)' * TC * 17.031;                    % g-at NH3/d, ammonia production
 
-    results = [Wg, Wg_d, J_E_Am, J_X_Am, J_f_Am, EJO, EJN];
-    data(:, i) = results; % pack results output
+    results = [Wg, Wg_d, J_E_Am, JT_XAm, J_f_Am, EJO, EJN];
+    data(:, i) = results;                             % pack results output
 end
