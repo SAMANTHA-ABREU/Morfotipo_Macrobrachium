ddcc68b6a563d8784913896a285c511df569cdaf
SAMANTHA-ABREU <66143734+SAMANTHA-ABREU@users.noreply.github.com>
2022-02-18 18:33:07 -0300

    Update predict_Macrobrachium_amazonicum.m

--- Amazonicum/P_m/predict_Macrobrachium_amazonicum.m
+++ Amazonicum/P_m/predict_Macrobrachium_amazonicum.m
@@ -46,7 +46,7 @@ function [prdData, info] = predict_Macrobrachium_amazonicum(par, data, auxData)
   % ultimate for Males CC
   k_MmCC = p_MCC/E_G;
   k_mCC = k_J/k_MmCC;
-  L_mmCC = v/ k_M/ g;                  % cm, max struct length
+  L_mmCC = v/ k_MmCC/ g;                  % cm, max struct length
   pars_tjmCC = [g k_mCC l_T v_Hb v_Hj v_Hp]; % parameter vector like pars_tj, but for males CC
   [t_jmCC, t_pmCC, t_bmCC, l_jmCC, l_pmCC, l_bmCC, l_imCC, rho_jmCC, rho_BmCC] = get_tj(pars_tjmCC, f);
   L_imCC = L_mmCC * l_imCC;     % cm, ultimate structural length at f
@@ -56,7 +56,7 @@ function [prdData, info] = predict_Macrobrachium_amazonicum(par, data, auxData)
   % ultimate for Males GC1
   k_MmGC1 = p_MGC1/E_G;
   k_mGC1 = k_J/k_MmGC1;
-  L_mmGC1 = v/ k_M/ g;                  % cm, max struct length
+  L_mmGC1 = v/ k_MmGC1/ g;                  % cm, max struct length
   pars_tjmGC1 = [g k_mGC1 l_T v_Hb v_Hj v_Hp]; % parameter vector like pars_tj, but for males GC1
   [t_jmGC1, t_pmGC1, t_bmGC1, l_jmGC1, l_pmGC1, l_bmGC1, l_imGC1, rho_jmGC1, rho_BmGC1] = get_tj(pars_tjmGC1, f);
   L_imGC1 = L_mmGC1 * l_imGC1;     % cm, ultimate structural length at f
@@ -66,7 +66,7 @@ function [prdData, info] = predict_Macrobrachium_amazonicum(par, data, auxData)
   % ultimate for Males GC2
   k_MmGC2 = p_MGC2/E_G;
   k_mGC2 = k_J/k_MmGC2;
-  L_mmGC2 = v/ k_M/ g;                  % cm, max struct length
+  L_mmGC2 = v/ k_MmGC2/ g;                  % cm, max struct length
   pars_tjmGC2 = [g k_mGC2 l_T v_Hb v_Hj v_Hp]; % parameter vector like pars_tj, but for males GC2
   [t_jmGC2, t_pmGC2, t_bmGC2, l_jmGC2, l_pmGC2, l_bmGC2, l_imGC2, rho_jmGC2, rho_BmGC2] = get_tj(pars_tjmGC2, f);
   L_imGC2 = L_mmGC2 * l_imGC2;     % cm, ultimate structural length at f
