221641869fc3b8444a9b31f57814bfe9259d2dda
SAMANTHA-ABREU <66143734+SAMANTHA-ABREU@users.noreply.github.com>
2022-03-16 14:40:40 -0300

    Update teste.m

--- Amazonicum/teste.m
+++ Amazonicum/teste.m
@@ -13,18 +13,14 @@ vars_pull(cPar);  %vars_pull(data);  vars_pull(auxData);
 pars_tj = [g k l_T v_Hb v_Hj v_Hp];
 [t_j, t_p, t_b, l_j, l_p, l_b, l_i, rho_j, rho_B, info] = get_tj(pars_tj, f);
 
-% Corrections
-t_experiment = 30;                                % d, experiment durantion (Augusto, 2016)
-t = (t_experiment);                         % d, estimated age (Augusto, 2016)
+% Corrections                          
+t_obs = 30;                         % d, experiment durantion (Augusto, 2016)
 Temp = C2K(30);
 TC = tempcorr(Temp, T_ref, T_A);
   
 L_p = L_m * l_p;                  % cm, structural length at puberty at f
 kT_M = k_M * TC;
 s_M=(l_j/l_b);                                    % Acceleration factor
-
-%ultimate length per morphotype(Moraes-Riodades, 2002)
-Lmax = [9.4, 9.4, 9.8, 13.0, 14.06];
 
 
 for i = 1:length(zoomfactor)
@@ -40,44 +36,39 @@ for i = 1:length(zoomfactor)
     pars_tj = [g k l_T v_Hb v_Hj v_Hp]; % parameter vector like pars_tj, but for males TC
     [t_j, t_p, t_b, l_j, l_p, l_b, l_i, rho_j, rho_B] = get_tj(pars_tj, f);   
     L_i = L_m * l_i;    % cm, ultimate structural length at f
-    
-    % Mass gained
     rT_B = rho_B * kT_M;
     
-    L_p_physical = L_p/del_M;
-    L_i_physical = L_i/del_M;
-    t_L =  log((L_p_physical-L_i_physical)/(Lmax(i)-L_i_physical))/rT_B; % d, Estimated growth time at f=1 for with morphotype from puberty to ultimate
-    t = linspace(t_p,t_L,len_resvector);
-    
-    L = L_i - (L_i-L_p) * exp(-rT_B*t);               % growth curve
-    Ww0 = L_p.^3 * (1 + f * ome);
-    Ww = L.^3 * (1 + f * ome);
-    Wg = Ww./Ww0;                                      % Mass gained
+    L_init = linspace(L_p,L_i,len_resvector);
+    t = t_p + 30;
 
-    % P - daily growth
-    Wg_d = (Ww-Ww0)./30;
+    % Mass gained (Augusto 2016 e 2014 - %)
+    L_f = L_i - (L_i-L_init) * exp(-rT_B*t_obs);                 % growth curve
+    Ww0 = L_init.^3 * (1 + f * ome);
+    Ww = L_f.^3 * (1 + f * ome);
+    Wg = Ww./Ww0;                                                % Mass gained (%)
 
-    % Ingestion rate
-    JT_X = p_Am * L.^2/ kap_X/ mu_X * w_X/ d_X * TC * s_M;  % g/d, ingested food (Fenneropenaeus chinensis)
+    % P - daily growth (Augusto 2016 - g/d e Augusto 2014 - mg/d)
+    Wg_d = (Ww-Ww0)./30;                                         % g/d
+
+    % Ingestion rate (Augusto 2016 - mg/d e Augusto 2014 - mg/d)
+    mu_X = 14000;                                                %J/gDW
+    X_wd = 2;                                                    %gWW/gDW
+    JT_X = 1000 * p_Am * L_f.^2/ kap_X/ mu_X * X_wd * TC * s_M;  % mg/d, ingested food (Fenneropenaeus chinensis)
     
-    % Faecal
-    JT_P = JT_X * (1 - kap_X);                         % g/d, max faeces production per surface area 
+    % Faecal (Augusto 2016 - mg e Augusto 2014 - mg)
+    JT_P = JT_X * (1 - kap_X);                                   % mg/d, max faeces production per surface area 
     
     % Flux data 
     pars_p = [kap; kap_R; g; k_J; k_M; L_T; v; U_Hb; U_Hj; U_Hp]; % compose pars
     p_ref = TC * p_Am * L_m^2;                        % J/d, max assimilation capacity at maximum size
-
-    % Respiration 
-    pACSJGRD = p_ref * scaled_power_j(L, f, pars_p, l_b, l_j, l_p); % J/d, powers
-    pACSJGRD(:,1) = 0 * pACSJGRD(:,1);                % exclude contribution from assimilation
+    pACSJGRD = p_ref * scaled_power_j(L_f, f, pars_p, l_b, l_j, l_p); % J/d, powers
     J_M = - (n_M\n_O) * eta_O * pACSJGRD(:, [1 7 5])';% mol/d: J_C, J_H, J_O, J_N in rows
-    EJO = - J_M(3,:)' * TC * 31.98;                   % g O2/d, O2 consumption
+    
+    % Respiration (Augusto 2016 - g e Augusto 2014 - g)
+    EJO = (- 2*16 * J_M(3,:) * TC./Ww)';                   % g O2/d, O2 consumption (Fenneropenaeus chinensis)
 
-    % Excretion
-    pACSJGRD = p_ref * scaled_power_j(L, f, pars_p, l_b, l_j, l_p); % J/d, powers
-    pACSJGRD(:,1) = 0 * pACSJGRD(:,1);                % exclude contribution from assimilation
-    J_M = - (n_M\n_O) * eta_O * pACSJGRD(:, [1 7 5])';% mol/d: J_C, J_H, J_O, J_N in rows
-    EJN = J_M(4,:)' * TC * 17.031;                    % g-at NH3/d, ammonia production
+    % Excretion (Augusto 2016 - mg e Augusto 2014 - mg)
+    EJN = (1000 * J_M(4,:) * TC * 17.031 ./ Ww)';                    % mg-at NH3/d, ammonia production
 
     results = struct;
     results.Wg = Wg;                                % pack results output
@@ -89,3 +80,87 @@ for i = 1:length(zoomfactor)
     
     save('Results_'+string(zoomfactor(i))+'.mat','results')
 end
+%%%Results%%%
+%Results Wg
+Wg = zeros(50,5);
+load ('Results_0.10152.mat')
+Wg (:,1) = results.Wg;
+load ('Results_0.10767.mat')
+Wg (:,2) = results.Wg;
+load ('Results_0.065596.mat')
+Wg (:,3) = results.Wg;
+load ('Results_0.075274.mat')
+Wg (:,4) = results.Wg;
+load ('Results_0.095441.mat')
+Wg (:,5) = results.Wg;
+csvwrite ( 'Wg.csv' , Wg)
+
+%Results Wg_d
+Wg_d = zeros(50,5);
+load ('Results_0.10152.mat')
+Wg_d (:,1) = results.Wg_d;
+load ('Results_0.10767.mat')
+Wg_d (:,2) = results.Wg_d;
+load ('Results_0.065596.mat')
+Wg_d (:,3) = results.Wg_d;
+load ('Results_0.075274.mat')
+Wg_d (:,4) = results.Wg_d;
+load ('Results_0.095441.mat')
+Wg_d (:,5) = results.Wg_d;
+csvwrite ( 'Wg_d.csv' , Wg_d)
+
+%Results JT_X
+JT_X = zeros(50,5);
+load ('Results_0.10152.mat')
+JT_X (:,1) = results.JT_X;
+load ('Results_0.10767.mat')
+JT_X (:,2) = results.JT_X;
+load ('Results_0.065596.mat')
+JT_X (:,3) = results.JT_X;
+load ('Results_0.075274.mat')
+JT_X (:,4) = results.JT_X;
+load ('Results_0.095441.mat')
+JT_X (:,5) = results.JT_X;
+csvwrite ( 'JT_X.csv' , JT_X)
+
+%Results JT_P
+JT_P = zeros(50,5);
+load ('Results_0.10152.mat')
+JT_P (:,1) = results.JT_P;
+load ('Results_0.10767.mat')
+JT_P (:,2) = results.JT_P;
+load ('Results_0.065596.mat')
+JT_P (:,3) = results.JT_P;
+load ('Results_0.075274.mat')
+JT_P (:,4) = results.JT_P;
+load ('Results_0.095441.mat')
+JT_P (:,5) = results.JT_P;
+csvwrite ( 'JT_P.csv' , JT_P)
+
+%Results EJO
+EJO = zeros(50,5);
+load ('Results_0.10152.mat')
+EJO (:,1) = results.EJO;
+load ('Results_0.10767.mat')
+EJO (:,2) = results.EJO;
+load ('Results_0.065596.mat')
+EJO (:,3) = results.EJO;
+load ('Results_0.075274.mat')
+EJO (:,4) = results.EJO;
+load ('Results_0.095441.mat')
+EJO (:,5) = results.EJO;
+csvwrite ( 'EJO.csv' , EJO)
+
+%Results EJN
+EJN = zeros(50,5);
+load ('Results_0.10152.mat')
+EJN (:,1) = results.EJN;
+load ('Results_0.10767.mat')
+EJN (:,2) = results.EJN;
+load ('Results_0.065596.mat')
+EJN (:,3) = results.EJN;
+load ('Results_0.075274.mat')
+EJN (:,4) = results.EJN;
+load ('Results_0.095441.mat')
+EJN (:,5) = results.EJN;
+csvwrite ( 'EJN.csv' , EJN)
